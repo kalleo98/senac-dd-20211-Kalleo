031ab1b8e6b2fbb1ce5537b0b7399541fe161d4d
Kalléo <Kallo@DESKTOP-E3NT135>
2021-03-01 10:25:39 -0300

    Projeto Maven com primeira classe criada

---
+++ src/main/java/view/PrimeiraTela.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PrimeiraTela extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrimeiraTela frame = new PrimeiraTela();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PrimeiraTela() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		JLabel lblOla = new JLabel("Ol\u00E1! Seja Bem-Vindo!");
+		lblOla.setForeground(Color.RED);
+		contentPane.add(lblOla, BorderLayout.CENTER);
+	}
+
+}
